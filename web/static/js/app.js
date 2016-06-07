3c523cd022444295cbe2aeaeba6ea3e204dc9c46
Shodai Yokoyama <quantumcars@gmail.com>
2016-06-07 21:58:03 +0900

    Embed client side app

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -11,7 +11,10 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
-import "phoenix_html"
+import "phoenix_html";
+
+const elmDiv = document.getElementById('elm-main')
+    , elmApp = Elm.Main.embed(elmDiv);
 
 // Import local files
 //
