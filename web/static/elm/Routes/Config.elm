73d7102e8d10f301930bc250d2a079e0e5fba06e
Shodai Yokoyama <quantumcars@gmail.com>
2016-06-22 08:19:12 +0900

    Routes: Add reverse

--- web/static/elm/Routes/Config.elm
+++ web/static/elm/Routes/Config.elm
@@ -1,6 +1,6 @@
 module Routes.Config exposing (..)
 
-import Hop exposing (makeUrl, makeUrlFromLocation, matchUrl, setQuery)
+import Hop exposing (makeUrl, makeUrlFromLocation, matchUrl, matcherToPath, setQuery)
 import Hop.Matchers exposing (..)
 import Hop.Types exposing (Config, Query, Location, PathMatcher, Router)
 import Navigation
@@ -30,3 +30,16 @@ matchers =
     [ topicsMatcher
     , topicMatcher
     ]
+
+
+reverse : Route -> String
+reverse route =
+    case route of
+        Topics ->
+            matcherToPath topicsMatcher []
+
+        Topic id ->
+            matcherToPath topicMatcher [ toString id ]
+
+        NotFound ->
+            ""
