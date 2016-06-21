531fb3e1ab589bfa5c560aad359b1cb1eb61a7a3
Shodai Yokoyama <quantumcars@gmail.com>
2016-06-21 22:01:43 +0900

    Routes: Define Routes

--- web/static/elm/Routes/Config.elm
+++ web/static/elm/Routes/Config.elm
@@ -4,3 +4,12 @@ import Hop exposing (makeUrl, makeUrlFromLocation, matchUrl, setQuery)
 import Hop.Matchers exposing (..)
 import Hop.Types exposing (Config, Query, Location, PathMatcher, Router)
 import Navigation
+
+
+-- ROUTES
+
+
+type Route
+    = Topics
+    | Topic Int
+    | NotFound
