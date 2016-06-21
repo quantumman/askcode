3fd6db0c5574fccd14e3432cf6077e805d9b2a79
Shodai Yokoyama <quantumcars@gmail.com>
2016-06-21 21:37:47 +0900

    Add Routes.elm

---
+++ web/static/elm/Routes/Config.elm
@@ -0,0 +1,6 @@
+module Routes.Config exposing (..)
+
+import Hop exposing (makeUrl, makeUrlFromLocation, matchUrl, setQuery)
+import Hop.Matchers exposing (..)
+import Hop.Types exposing (Config, Query, Location, PathMatcher, Router)
+import Navigation
