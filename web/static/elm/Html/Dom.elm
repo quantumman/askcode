5bc84dfc63382af795bc007762a2b324d3fee20e
Shodai Yokoyama <quantumcars@gmail.com>
2016-06-26 02:15:06 +0900

    Dom: Add hooks to detect dom changed

--- web/static/elm/Html/Dom.elm
+++ web/static/elm/Html/Dom.elm
@@ -1,1 +1,19 @@
 port module Html.Dom exposing (..)
+
+-- UPDATE
+
+
+port observe : Id -> Cmd msg
+
+
+port disconnect : Id -> Cmd msg
+
+
+
+-- SUBSCRIPTION
+
+
+port addDom : (Id -> msg) -> Sub msg
+
+
+port removeDom : (Id -> msg) -> Sub msg
