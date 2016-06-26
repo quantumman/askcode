174fcafebb52b0d29e57829f41e89c1348bc26d6
Shodai Yokoyama <quantumcars@gmail.com>
2016-06-27 08:12:00 +0900

    app.js: Add hook functions for dom changes

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -14,57 +14,118 @@
 import "phoenix_html";
 import Elm from "../../static/elm/Main.elm";
 import Ace from 'brace';
+import Lazy from 'lazy.js';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import '../../static/css/app.css';
 
+
 const elmDiv = document.getElementById("elm-main")
     , elmApp = Elm.Main.embed(elmDiv);
 
-const onNodeMutated = (id, parent, selector, callback) => {
-  const observer = new MutationObserver(mutations => {
-    const mutatedIds = mutations.reduce((acc, mutationRecord) => {
-      const ids = Array
-        .from(selector(mutationRecord))
-        .filter(node => node.attributes.getNamedItem('id') !== null)
-        .map(node => node.attributes.getNamedItem('id').value);
-      return acc.concat(ids);
-    }, []);
+const flatten = nodeList => {
+  if (!nodeList) {
+    return [];
+  }
 
-    if (mutatedIds.indexOf(id) >= 0) {
-      observer.disconnect();
-      callback(mutatedIds);
-    }
-  });
-
-  observer.observe(parent, {
-    childList: true,
-    subtree: true
-  });
+  const nodes = Lazy(Array.from(nodeList));
+  const childNodes = nodes
+    .filter(node => typeof node.childNodes !== 'undefined')
+    .map(node => flatten(node.childNodes).toArray())
+    .flatten();
+  return nodes.concat(childNodes);
 };
 
-onNodeMutated('elm-main-app', elmDiv, m => m.addedNodes, ids => {
-  elmApp.ports.ready.send('ready');
-});
+const observe = (id, selector, callback) => {
+  const observer = new MutationObserver((mutations, self) => {
+    const found = Lazy(mutations)
+      .map(mutation => flatten(selector(mutation)))
+      .flatten()
+      .filter(node => typeof node.attributes !== 'undefined')
+      .filter(node => node.attributes.getNamedItem('id') !== null)
+      .map(node => node.attributes.getNamedItem('id').value)
+      .find(x => x === id);
+    callback(found, self);
+  });
 
+  observer.observe(elmDiv, {
+    childList: true,
+    subtree: true,
+  });
+
+  return observer;
+};
+
+const observeOn = id => {
+  const observeDomRemoved = targetId =>
+    observe(targetId, m => m.removedNodes, (removedId, self) => {
+      if (removedId) {
+        elmApp.ports.removeDom.send(removedId);
+        self.disconnect();
+      }
+    });
+
+  const sendDomAdded = target => {
+    if (target) {
+      const id = target.id || target;
+      setTimeout(() => elmApp.ports.addDom.send(id), 0);
+      observeDomRemoved(id);
+    }
+  };
+
+  const element = document.getElementById(id);
+  if (element) {
+    sendDomAdded(element);
+    return undefined;
+  }
+  else {
+    return observe(id, m => m.addedNodes, addedId => {
+      sendDomAdded(addedId);
+    });
+  }
+};
+
+
+// Ace editor
 const editor = {};
-elmApp.ports.initialize.subscribe(([id, text]) => {
-  if (id && editor[id] === undefined) {
-    editor[id] = Ace.edit(id);
-
+const initialize = (element, text) => {
+  const id = element.id;
+  if (!editor[id]) {
+    editor[id] = Ace.edit(element);
     editor[id].setValue(text);
     editor[id].on('change', e => {
       elmApp.ports.change.send([id, editor[id].getValue()]);
     });
-
-    onNodeMutated(id, elmDiv, m => m.removedNodes, ids => {
-      if (editor[id]) {
-        editor[id].destroy();
-        delete editor[id];
-      }
-    });
+  }
+};
+elmApp.ports.initialize.subscribe(([id, text]) => {
+  var target = document.getElementById(id);
+  if (target) {
+    initialize(target, text);
   }
 });
+elmApp.ports.destroy.subscribe(id => {
+  if (editor[id]) {
+    editor[id].destroy();
+    delete editor[id];
+  }
+});
+
+
+const observers = {};
+elmApp.ports.observe.subscribe(id => {
+  observers[id] = observeOn(id);
+});
+elmApp.ports.disconnect.subscribe(id => {
+  if (observers[id]) {
+    observers[id].disconnect(id);
+    delete observers[id];
+  }
+});
+
+
+
+
 
 // Import local files
 //
