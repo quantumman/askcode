793e053089d8b3cc55406c06f20646e7a1c0ef4f
Shodai Yokoyama <quantumcars@gmail.com>
2016-06-27 22:48:50 +0900

    Routes: Add matchers

--- web/static/elm/Routes/Page/Config.elm
+++ web/static/elm/Routes/Page/Config.elm
@@ -15,3 +15,32 @@ type Route
     | Index
     | Show Int
     | NotFound
+
+
+newMatcher : PathMatcher Route
+newMatcher =
+    match1 New "/new"
+
+
+editMatcher : PathMatcher Route
+editMatcher =
+    match2 Edit "/edit/" int
+
+
+indexMatcher : PathMatcher Route
+indexMatcher =
+    match1 Index ""
+
+
+showMatcher : PathMatcher Route
+showMatcher =
+    match2 Show "/show/" int
+
+
+matchers : List (PathMatcher Route)
+matchers =
+    [ newMatcher
+    , editMatcher
+    , indexMatcher
+    , showMatcher
+    ]
