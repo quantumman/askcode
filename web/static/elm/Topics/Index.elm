bbd8888eb3f56fbcd72436d6149db5d93e0ff1ca
Shodai Yokoyama <quantumcars@gmail.com>
2016-06-30 00:47:08 +0900

    Topics: Add Index

---
+++ web/static/elm/Topics/Index.elm
@@ -0,0 +1,15 @@
+module Topics.Index exposing (..)
+
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Topics.Model exposing (..)
+
+
+-- VIEW
+
+
+view : Model -> Html msg
+view model =
+    div []
+        [ text "test" ]
