47ea3e586531a912db922f8980f58c397bcb8ed6
Shodai Yokoyama <quantumcars@gmail.com>
2016-06-29 09:28:17 +0900

    Put Root component in different module

--- web/static/elm/Main.elm
+++ web/static/elm/Main.elm
@@ -2,13 +2,10 @@ module Main exposing (..)
 
 import App
 import Hop exposing (makeUrl, makeUrlFromLocation, matchUrl, setQuery)
-import Hop.Matchers exposing (..)
 import Hop.Types exposing (Config, Query, Location, PathMatcher, Router)
-import Html exposing (..)
-import Html.App as Html
 import Navigation
-import Routing.Config exposing (..)
-import Topics.View as Topics
+import Root exposing (..)
+import Routing.Config exposing (urlParser, init, make, Route)
 
 
 -- APP
@@ -38,68 +35,8 @@ main : Program Never
 main =
     Navigation.program Routing.Config.urlParser
         { init = init
-        , view = dispatcher
+        , view = view
         , update = update
         , urlUpdate = urlUpdate
         , subscriptions = subscriptions
         }
-
-
-
--- MODEL
-
-
-type alias Model =
-    { routes : Routing.Config.Model
-    , app : App.Model
-    }
-
-
-
--- UPDATE
-
-
-type Msg
-    = NavigateTo Routing.Config.Msg
-    | App App.Msg
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NavigateTo subMessage ->
-            let
-                ( routes, command ) =
-                    Routing.Config.update subMessage model.routes
-            in
-                ( { model | routes = routes }, Cmd.map NavigateTo command )
-
-        App subMessage ->
-            let
-                ( app, command ) =
-                    App.update subMessage model.app
-            in
-                ( { model | app = app }, Cmd.map App command )
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.map App (App.subscriptions model.app)
-
-
-
--- VIEWS
-
-
-dispatcher : Model -> Html Msg
-dispatcher model =
-    case model.routes.route of
-        Topics subRoute ->
-            Topics.view subRoute
-
-        NotFound ->
-            div [] [ h2 [] [ text "Not Found " ] ]
--- web/static/elm/Root.elm
+++ web/static/elm/Root.elm
@@ -1,2 +1,66 @@
 module Root exposing (..)
 
+import App
+import Html exposing (..)
+import Html.App as Html
+import Routing.Config as Routing exposing (..)
+import Topics.View as Topics
+
+
+-- MODEL
+
+
+type alias Model =
+    { routes : Routing.Model
+    , app : App.Model
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NavigateTo Routing.Msg
+    | App App.Msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NavigateTo subMessage ->
+            let
+                ( routes, command ) =
+                    Routing.update subMessage model.routes
+            in
+                ( { model | routes = routes }, Cmd.map NavigateTo command )
+
+        App subMessage ->
+            let
+                ( app, command ) =
+                    App.update subMessage model.app
+            in
+                ( { model | app = app }, Cmd.map App command )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.map App (App.subscriptions model.app)
+
+
+
+-- VIEWS
+
+
+view : Model -> Html Msg
+view model =
+    case model.routes.route of
+        Topics subRoute ->
+            Topics.view subRoute
+
+        NotFound ->
+            div [] [ h2 [] [ text "Not Found " ] ]
