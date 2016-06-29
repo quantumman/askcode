ae1c2977634224c1db4dd837d4edade953424d1d
Shodai Yokoyama <quantumcars@gmail.com>
2016-06-30 00:40:27 +0900

    Topics: Model - Add Proposal

--- web/static/elm/Topics/Model.elm
+++ web/static/elm/Topics/Model.elm
@@ -5,3 +5,14 @@ type alias User =
     { avatar : String
     , name : String
     }
+
+
+type alias Proposal =
+    { id : Int
+    , description : String
+    , code : String
+    , votes : Int
+    , user : User
+    , created_at : String
+    , updated_at : String
+    }
