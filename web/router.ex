774bd4e97b5dc2d1a9a58dce78249676b31795ba
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-04 09:20:48 +0900

    Scaffold users/

--- web/router.ex
+++ web/router.ex
@@ -19,8 +19,9 @@ defmodule Askcode.Router do
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", Askcode do
-  #   pipe_through :api
-  # end
+  scope "/api", Askcode do
+    pipe_through :api
+
+    resources "/users", UserController, except: [:new, :edit]
+  end
 end
