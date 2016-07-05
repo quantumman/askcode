4bb3ffce6c104ffb59ffb5192540cfc5c93e3b50
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-05 21:49:06 +0900

    forge: Generate discussion

--- lib/forge.ex
+++ lib/forge.ex
@@ -4,4 +4,9 @@ defmodule Forge do
   @save_one_function &Blacksmith.Config.save/2
   @save_all_function &Blacksmith.Config.save_all/2
 
+  register :discussion, %Askcode.Discussion{
+    subject: Faker.App.name,
+    description: Faker.Lorem.Shakespeare.hamlet,
+    code: Faker.Lorem.sentence(%Range{first: 1, last: 10})
+  }
 end
