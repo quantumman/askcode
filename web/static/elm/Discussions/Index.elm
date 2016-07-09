316746d3cf33d3f35af0a4b4536d5683ab1a6780
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-09 10:37:06 +0900

    Discussions: Index - Show a list of Discussions

--- web/static/elm/Discussions/Index.elm
+++ web/static/elm/Discussions/Index.elm
@@ -3,13 +3,44 @@ module Discussions.Index exposing (..)
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
+import Style exposing (..)
+import Styles exposing (..)
 import Discussions.Model exposing (..)
 
 
 -- VIEW
 
 
-view : List Discussion -> Html msg
+view : List Discussion -> Html Msg
 view model =
     div []
-        [ text "test" ]
+        [ div [ class "row" ]
+            (discussions model)
+        ]
+
+
+discussions : List Discussion -> List (Html msg)
+discussions discussions =
+    let
+        item discussion =
+            let
+                length =
+                    List.length discussion.replies
+            in
+                div [ class "media" ]
+                    [ a [ class "media-left", href "#" ]
+                        [ img
+                            [ class "media-object img-circle"
+                            , src discussion.creator.avatar
+                            , alt "(^x^)"
+                            ]
+                            []
+                        ]
+                    , div [ class "media-body" ]
+                        [ h4 [ class "media-heading" ]
+                            [ text discussion.subject ]
+                        , text discussion.description
+                        ]
+                    ]
+    in
+        List.map item discussions
