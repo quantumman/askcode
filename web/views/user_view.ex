c9d3a578363b3586861913bcb49ab241fd2155e0
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-12 07:36:21 +0900

    user_view: Remove envelope

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -2,11 +2,11 @@ defmodule Askcode.UserView do
   use Askcode.Web, :view
 
   def render("index.json", %{users: users}) do
-    %{data: render_many(users, Askcode.UserView, "user.json")}
+    render_many(users, Askcode.UserView, "user.json")
   end
 
   def render("show.json", %{user: user}) do
-    %{data: render_one(user, Askcode.UserView, "user.json")}
+    render_one(user, Askcode.UserView, "user.json")
   end
 
   def render("user.json", %{user: user}) do
