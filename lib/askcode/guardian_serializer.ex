1022add2d2201aa3809af82b30e055899d9d5451
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-12 08:13:47 +0900

    lib: Add guardian_serializer

---
+++ lib/askcode/guardian_serializer.ex
@@ -0,0 +1,12 @@
+defmodule Askcode.GuardianSerializer do
+  @behaviour Guardian.Serializer
+
+  alias Askcode.Repo
+  alias Askcode.User
+
+  def for_token(user = %User{}), do: { :ok, "User:#{user.id}" }
+  def for_token(_), do: { :error, "Unknown resource type" }
+
+  def from_token("User:" <> id), do: { :ok, Repo.get(User, id) }
+  def from_token(_), do: { :error, "Unknown resource type" }
+end
