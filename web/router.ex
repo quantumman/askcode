8c191676cbc79052cbeb524cee9eb7a3b15dea76
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-12 08:16:46 +0900

    router: Verify authentication

--- web/router.ex
+++ web/router.ex
@@ -11,6 +11,8 @@ defmodule Askcode.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.LoadResource
   end
 
   scope "/", Askcode do
