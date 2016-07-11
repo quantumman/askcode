814d135dca80682a8e93b194479db5109fd7d5ca
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-11 21:17:31 +0900

    Discussions: Add views

--- web/static/elm/Discussions.elm
+++ web/static/elm/Discussions.elm
@@ -1,6 +1,10 @@
 module Discussions exposing (..)
 
 import Discussions.Model exposing (Model)
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Routing.Page.Config exposing (..)
 
 
 -- UPDATE
@@ -27,3 +31,26 @@ update message model =
 
         ShowMsg ->
             ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Route -> Model -> Html Msg
+view route model =
+    case route of
+        New ->
+            div [] [ text "new" ]
+
+        Edit id ->
+            div [] [ text "edit" ]
+
+        Index ->
+            div [] [ text "index" ]
+
+        Show id ->
+            div [] [ text "show" ]
+
+        NotFound ->
+            div [] [ text "NotFound" ]
