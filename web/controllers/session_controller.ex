5359ef95656b167dbcb6187c780b499d4c4b6294
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-12 09:50:44 +0900

    session_controller: Add create action

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,4 +1,21 @@
-defmodule PhoenixTrello.SessionController do
-  use PhoenixTrello.Web, :controller
+defmodule Askcode.SessionController do
+  use Askcode.Web, :controller
 
+  plug :scrub_params, "session" when action in [:create]
+
+  def create(conn, %{"session" => session_params}) do
+    case Askcode.Session.authenticate(session_params) do
+      {:ok, user} ->
+        {:ok, jwt, _full_claims} = user |> Guardian.encode_and_sign(:token)
+
+        conn
+        |> put_status(:created)
+        |> render("show.json", jwt: jwt, user: user)
+
+      :error ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render("error.json")
+    end
+  end
 end
