e0bdd40d5cc841aca55c47a8bed2c50c5836d9c4
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-12 20:07:11 +0900

    user: Transform password

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -52,4 +52,19 @@ defmodule Askcode.UserController do
 
     send_resp(conn, :no_content, "")
   end
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> User.changeset
+    |> generate_encrypted_password
+  end
+
+  defp generate_encrypted_password(current_changeset) do
+    case current_changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        Ecto.Changeset.put_change(current_changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(password))
+      _ ->
+        current_changeset
+    end
+  end
 end
