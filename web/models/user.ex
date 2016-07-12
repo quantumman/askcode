dfb8c96d1cfdd65c50de02551906eae7b8b5c923
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-12 09:38:17 +0900

    user: Add password field as virtual

--- web/models/user.ex
+++ web/models/user.ex
@@ -6,6 +6,7 @@ defmodule Askcode.User do
     field :email, :string
     field :avatar, :string
     field :encrypted_password, :string
+    field :password, :string, virtual: true
 
     has_many :replies, Askcode.Reply
     has_many :discussions, Askcode.Discussion
