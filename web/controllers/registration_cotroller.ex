eacf4da11527569e38102f96be9c7d9c3a9d9533
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-12 20:10:59 +0900

    registration_controller: Sign up user

--- web/controllers/registration_cotroller.ex
+++ web/controllers/registration_cotroller.ex
@@ -1,3 +1,25 @@
 defmodule Askcode.RegistrationController  do
   use Askcode.Web, :controller
+
+  alias Askcode.{Repo, User}
+
+  plug :scrub_params, "user" when action in [:create]
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        {:ok, jwt, _full_claims} = Guardian.encode_and_sign(user, :token)
+
+        conn
+        |> put_status(:created)
+        |> render(Askcode.SessionView, "show.json", jwt: jwt, user: user)
+
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(Askcode.ChangesetView, "error.json", changeset: changeset)
+    end
+  end
 end
