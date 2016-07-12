2d6fa86ab138f0bdcbb1e6c71a90a31ab9013088
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-12 22:27:56 +0900

    SignUp: Add Credential

--- web/static/elm/SignUp.elm
+++ web/static/elm/SignUp.elm
@@ -1,9 +1,18 @@
 module SignUp exposing (..)
 
+import Models exposing (..)
+
+
 -- MODEL
 
 
 type alias Model =
     { email : String
     , password : String
+    }
+
+
+type alias Credential =
+    { jwt : String
+    , user : User
     }
