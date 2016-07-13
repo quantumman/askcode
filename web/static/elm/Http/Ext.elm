478006ef5e106a8a60125619be571a2d7bded509
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-14 08:51:51 +0900

    Html.Ext: Add post' to send json payload

--- web/static/elm/Http/Ext.elm
+++ web/static/elm/Http/Ext.elm
@@ -1,2 +1,17 @@
 module Http.Ext exposing (..)
 
+import Http
+import Json.Decode as Decode exposing (..)
+import Json.Encode as Encode exposing (..)
+import Task exposing (Task)
+
+
+post' : Decoder a -> String -> Encode.Value -> Task Http.Error a
+post' decoder url payload =
+    Http.send Http.defaultSettings
+        { verb = "POST"
+        , headers = [ ( "Content-Type", "application/json" ) ]
+        , url = Http.url url []
+        , body = Http.string (Encode.encode 0 payload)
+        }
+        |> Http.fromJson decoder
