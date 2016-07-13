e48ca96af7ee230f59578fcc6f270c1a2d683e85
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-14 08:31:27 +0900

    SignIn: Add Msg

--- web/static/elm/SignIn.elm
+++ web/static/elm/SignIn.elm
@@ -1,9 +1,25 @@
 module SignIn exposing (..)
 
+import Http
+import Models exposing (..)
+
 
 -- MODEL
+
 
 type alias Model =
     { email : String
     , password : String
     }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = SignIn
+    | SignInSuccess Credential
+    | SignInFail Http.Error
+    | EmailOrUserName String
+    | Password String
