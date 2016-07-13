060871c315e978cda616142ba3d9c9af18ac2a64
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-14 08:02:34 +0900

    Helpers: Add row

--- web/static/elm/Html/Helpers.elm
+++ web/static/elm/Html/Helpers.elm
@@ -1,1 +1,10 @@
 module Html.Helpers exposing (..)
+
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+
+
+row : List (Html msg) -> Html msg
+row content =
+    div [ class "row" ] content
