15dffaf0a01722ceda88bcaecbe1dd5b0cc68423
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-15 07:25:39 +0900

    Html.Events.Ext: Add onEnter

--- web/static/elm/Html/Events/Ext.elm
+++ web/static/elm/Html/Events/Ext.elm
@@ -1,1 +1,18 @@
 module Html.Events.Ext exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Json.Decode as Decode exposing (..)
+
+
+onEnter : msg -> msg -> Attribute msg
+onEnter noop msg =
+    let
+        tagger code =
+            if code == 13 then
+                msg
+            else
+                noop
+    in
+        on "keydown" (Decode.map tagger keyCode)
