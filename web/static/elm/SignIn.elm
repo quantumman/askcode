ce003e33715d5d4a1211d3d5f37ccd232394f9c1
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-15 07:39:30 +0900

    SignIn: Add update

--- web/static/elm/SignIn.elm
+++ web/static/elm/SignIn.elm
@@ -27,6 +27,25 @@ type Msg
     | Password String
 
 
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        SignIn ->
+            model ! [ signIn model ]
+
+        SignInSuccess response ->
+            model ! []
+
+        SignInFail error ->
+            model ! []
+
+        EmailOrUserName email ->
+            { model | email = email } ! []
+
+        Password password ->
+            { model | password = password } ! []
+
+
 signIn : Model -> Cmd Msg
 signIn model =
     let
