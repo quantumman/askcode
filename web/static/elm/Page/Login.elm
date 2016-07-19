479e9cb52fd426befdd28a452e458998b3d48b32
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-20 08:07:52 +0900

    Login: Show SignIn form

--- web/static/elm/Page/Login.elm
+++ web/static/elm/Page/Login.elm
@@ -1,1 +1,48 @@
 module Page.Login exposing (..)
+
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Helpers as Html exposing (..)
+import Page.UI.SignIn as SignIn
+
+
+-- MODEL
+
+
+type alias Model =
+    { signIn : SignIn.Model
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = SignIn SignIn.Msg
+
+
+update : Msg -> Model -> Cmd Msg
+update message model =
+    case message of
+        SignIn subMessage ->
+            let
+                ( _, subCommand ) =
+                    SignIn.update subMessage model.signIn
+            in
+                Cmd.map SignIn subCommand
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    let
+        signIn =
+            Html.map SignIn (SignIn.view model.signIn)
+    in
+        div []
+            [ signIn ]
