206effa0772def359dea34505864796ac9951d47
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-21 08:29:34 +0900

    Session: Add load

--- web/static/elm/Http/Session.elm
+++ web/static/elm/Http/Session.elm
@@ -1,8 +1,10 @@
 module Http.Session exposing (..)
 
+import Json.Decode as Decode exposing (..)
 import Json.Encode as Encode exposing (..)
 import LocalStorage exposing (..)
 import Models exposing (..)
+import Result exposing (Result)
 import Task exposing (Task)
 
 
@@ -31,4 +33,13 @@ store fmsg credential =
         |> Encode.encode 0
         |> set key
         |> Task.perform StoreFail StoreSuccess
+        |> Cmd.map fmsg
+
+
+load : (Msg -> msg) -> Cmd msg
+load fmsg =
+    get key
+        |> Task.map (Decode.decodeString decodeCredential)
+        |> Task.map Result.toMaybe
+        |> Task.perform LoadFail LoadSuccess
         |> Cmd.map fmsg
