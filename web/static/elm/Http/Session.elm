67fd56dc5e5d2bd481cd76295bf55d3599b4add3
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-22 08:10:31 +0900

    Session: Add Msg

--- web/static/elm/Http/Session.elm
+++ web/static/elm/Http/Session.elm
@@ -1,1 +1,14 @@
 module Http.Session exposing (..)
+
+import LocalStorage exposing (..)
+import Models exposing (..)
+
+
+-- MESSAGE
+
+
+type Msg
+    = StoreSuccess ()
+    | StoreFail Error
+    | LoadSuccess (Maybe Credential)
+    | LoadFail Error
