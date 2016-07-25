9a19e36b0988cca317e55173a8f011218e7d6a16
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-26 08:06:58 +0900

    Alert: Add Model

--- web/static/elm/Page/UI/Alert.elm
+++ web/static/elm/Page/UI/Alert.elm
@@ -1,1 +1,17 @@
 module Page.UI.Alert exposing (..)
+
+-- MODEL
+
+
+type Model
+    = Success String
+    | Info String
+    | Warning String
+    | Error String
+    | Note String
+    | Dismiss
+
+
+init : Model
+init =
+    Dismiss
