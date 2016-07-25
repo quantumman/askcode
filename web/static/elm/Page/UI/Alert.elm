d7867c59d9f01462f4c344783184d0db80ea67d1
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-26 08:50:45 +0900

    Alert: Add update'

--- web/static/elm/Page/UI/Alert.elm
+++ web/static/elm/Page/UI/Alert.elm
@@ -30,3 +30,15 @@ update message model =
     case message of
         Close ->
             ( Dismiss, Cmd.none )
+
+
+update' : Msg -> { model | alert : Model } -> (Msg -> msg) -> ( { model | alert : Model }, Cmd msg )
+update' message rootModel fmsg =
+    let
+        model =
+            rootModel.alert
+
+        ( model', command ) =
+            update message model
+    in
+        ( { rootModel | alert = model' }, Cmd.map fmsg command )
