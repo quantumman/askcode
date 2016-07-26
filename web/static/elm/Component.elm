7411595ce502f739e3eda734ccb2f4589938ed2f
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-27 06:59:43 +0900

    Component: Add second

--- web/static/elm/Component.elm
+++ web/static/elm/Component.elm
@@ -7,3 +7,8 @@ module Component exposing (..)
 first : (a -> b) -> ( a, c ) -> ( b, c )
 first f ( x, y ) =
     ( f x, y )
+
+
+second : (b -> c) -> ( a, b ) -> ( a, c )
+second f ( x, y ) =
+    ( x, f y )
