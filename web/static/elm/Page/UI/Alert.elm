a81d6bc406a29bded69c251fb567dd10060e380e
Shodai Yokoyama <quantumcars@gmail.com>
2016-07-28 14:35:45 +0900

    Alert: Remove unused code

--- web/static/elm/Page/UI/Alert.elm
+++ web/static/elm/Page/UI/Alert.elm
@@ -39,18 +39,6 @@ update message model =
             ( Dismiss, Cmd.none )
 
 
-update' : Msg -> { model | alert : Model } -> (Msg -> msg) -> ( { model | alert : Model }, Cmd msg )
-update' message rootModel fmsg =
-    let
-        model =
-            rootModel.alert
-
-        ( model', command ) =
-            update message model
-    in
-        ( { rootModel | alert = model' }, Cmd.map fmsg command )
-
-
 
 -- VIEW
 
