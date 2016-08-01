08c6239ec37b1fe40b6cc3d538a18385d0aa022e
Shodai Yokoyama <quantumcars@gmail.com>
2016-08-01 11:58:33 +0900

    Form: Add form

--- web/static/elm/View/Form.elm
+++ web/static/elm/View/Form.elm
@@ -1,1 +1,15 @@
 module View.Form exposing (..)
+
+import Form exposing (Form)
+import Form.Input as Input
+import Form.Validate as Validate exposing (..)
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+import Html.Events.Ext exposing (..)
+
+
+form : List (Html Form.Msg) -> Html Form.Msg
+form =
+    Html.form [ onEnter Form.NoOp Form.Submit ]
