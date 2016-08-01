6c5a327ea071a05c448a589e1d46ff09b4e241e8
Shodai Yokoyama <quantumcars@gmail.com>
2016-08-02 06:40:23 +0900

    Layout: Add column

--- web/static/elm/View/Layout.elm
+++ web/static/elm/View/Layout.elm
@@ -8,3 +8,8 @@ import Html.Attributes exposing (..)
 row : List (Html msg) -> Html msg
 row content =
     div [ class "row" ] content
+
+
+column : Int -> List (Html msg) -> Html msg
+column n content =
+    div [ class ("col-sm-" ++ (toString n)) ] content
