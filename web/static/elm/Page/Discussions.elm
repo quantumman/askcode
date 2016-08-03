5a9542c38c72985da14138e5ee57365746da2b22
Shodai Yokoyama <quantumcars@gmail.com>
2016-08-04 06:37:24 +0900

    elm: Page - Add Discussions Page

---
+++ web/static/elm/Page/Discussions.elm
@@ -0,0 +1,85 @@
+module Page.Discussions exposing (..)
+
+import Discussions.Index as Index exposing (..)
+import Discussions.Model as Model exposing (..)
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Routing.Page.Config exposing (..)
+
+
+-- MODEL
+
+
+type alias Model =
+    { index : List Model.Discussion
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    let
+        model =
+            { index = []
+            }
+
+        command =
+            Cmd.batch
+                [ Cmd.map IndexMsg Index.fetchCommand
+                ]
+    in
+        ( model, command )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NewMsg
+    | EditMsg
+    | IndexMsg Index.Msg
+    | ShowMsg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        NewMsg ->
+            ( model, Cmd.none )
+
+        EditMsg ->
+            ( model, Cmd.none )
+
+        IndexMsg subMessage ->
+            let
+                ( model', command ) =
+                    Index.update subMessage model.index
+            in
+                ( { model | index = model' }, Cmd.map IndexMsg command )
+
+        ShowMsg ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Route -> Model -> Html Msg
+view route model =
+    case route of
+        New ->
+            div [] [ text "new" ]
+
+        Edit id ->
+            div [] [ text "edit" ]
+
+        Index ->
+            Html.map IndexMsg (Index.view model.index)
+
+        Show id ->
+            div [] [ text "show" ]
+
+        NotFound ->
+            div [] [ text "NotFound" ]
---
+++ web/static/elm/Page/Discussions/Index.elm
@@ -0,0 +1,91 @@
+module Page.Discussions.Index exposing (..)
+
+import Discussions.Model exposing (..)
+import Html exposing (..)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Http
+import Json.Decode as Decode exposing (..)
+import Style exposing (..)
+import Styles exposing (..)
+import Task exposing (Task)
+
+
+-- UPDATE
+
+
+type Msg
+    = Fetch
+    | FetchSuccess (List Discussion)
+    | FetchError Http.Error
+
+
+update : Msg -> List Discussion -> ( List Discussion, Cmd Msg )
+update message model =
+    case message of
+        Fetch ->
+            ( model, fetchCommand )
+
+        FetchSuccess model' ->
+            ( model', Cmd.none )
+
+        FetchError _ ->
+            ( model, Cmd.none )
+
+
+fetchCommand : Cmd Msg
+fetchCommand =
+    let
+        decode =
+            Decode.list
+                (Decode.object5 (\a b c d e -> Discussion a b c [] (User d e))
+                    ("subject" := Decode.string)
+                    ("description" := Decode.string)
+                    ("code" := Decode.string)
+                    (Decode.at ["creator", "avatar"] Decode.string)
+                    (Decode.at ["creator", "name"] Decode.string)
+                )
+
+        task =
+            Http.get decode "/api/discussions"
+    in
+        Task.perform FetchError FetchSuccess task
+
+
+
+-- VIEW
+
+
+view : List Discussion -> Html Msg
+view model =
+    div []
+        [ div [ class "row" ]
+            (discussions model)
+        ]
+
+
+discussions : List Discussion -> List (Html msg)
+discussions discussions =
+    let
+        item discussion =
+            let
+                length =
+                    List.length discussion.replies
+            in
+                div [ class "media" ]
+                    [ a [ class "media-left", href "#" ]
+                        [ img
+                            [ class "media-object img-circle"
+                            , src discussion.creator.avatar
+                            , alt "(^x^)"
+                            ]
+                            []
+                        ]
+                    , div [ class "media-body" ]
+                        [ h4 [ class "media-heading" ]
+                            [ text discussion.subject ]
+                        , text discussion.description
+                        ]
+                    ]
+    in
+        List.map item discussions
