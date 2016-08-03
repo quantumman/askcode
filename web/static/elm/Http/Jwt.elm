5b19fd21b8c9e95361bbe38801555528873b687c
Shodai Yokoyama <quantumcars@gmail.com>
2016-08-03 20:44:05 +0900

    Http.Jwt: Add andThen

--- web/static/elm/Http/Jwt.elm
+++ web/static/elm/Http/Jwt.elm
@@ -1,1 +1,31 @@
 module Http.Jwt exposing (..)
+
+import Http
+import Http.Extra exposing (..)
+import Http.Session as Session exposing (..)
+import Json.Decode as Decode exposing (..)
+import Json.Encode as Encode exposing (..)
+import Jwt exposing (..)
+import Models exposing (..)
+import Task exposing (Task)
+
+
+type Error
+    = SessionError Session.Error
+    | EmptySessionError String
+    | HttpError Http.Error
+
+
+andThen : Task Session.Error (Maybe Credential) -> (Credential -> Task Http.Error a) -> Task Error a
+andThen m f =
+    let
+        m' =
+            Task.mapError SessionError m
+
+        jwt' =
+            Task.fromMaybe (EmptySessionError "Credential is empty")
+
+        f' x =
+            Task.mapError HttpError (f x)
+    in
+        m' `Task.andThen` jwt' `Task.andThen` f'
