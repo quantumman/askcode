d4b0ca14906fe4ecaebcc659b3e07fcc3017bb61
Shodai Yokoyama <quantumcars@gmail.com>
2016-08-09 10:11:19 +0900

    Popover: Add a style for popover

--- web/static/elm/Page/UI/Popover.elm
+++ web/static/elm/Page/UI/Popover.elm
@@ -1,8 +1,37 @@
 module Page.UI.Popover exposing (..)
 
+import Html exposing (..)
+import Html.App as Html
+import Html.Events exposing (..)
+import Style exposing (..)
+import Styles exposing (..)
+
+
 -- UPDATE
 
 
 type Msg
     = Open
     | Close
+
+
+
+--- STYLE
+
+
+popover : List Style
+popover =
+    [ position absolute
+    , top "100%"
+    , right "0"
+    , zIndex "100"
+    , marginLeft "-10em"
+    , display block
+    , width <| Style.em 20
+    , padding <| Style.em 0.3
+    , ( "word-wrap", "break-word" )
+    , ( "background-clip", "padding-box" )
+    , border "1px solid #EEEEEE"
+    , backgroundColor "#FFFFFF"
+    , borderRadius "5px 5px 5px 5px"
+    ]
